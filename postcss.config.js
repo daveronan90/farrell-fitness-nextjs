3da241677ec02c667cb6ddc0a45d7eadc06f8172
daveronan90 <daveronan90@gmail.com>
2021-10-28 20:38:38 +0100

    Trial 100vh mobile fix 3

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,5 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
-  plugins: {
-    "postcss-100vh-fix": {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: ["tailwindcss", "autoprefixer", "postcss-100vh-fix"],
 };
